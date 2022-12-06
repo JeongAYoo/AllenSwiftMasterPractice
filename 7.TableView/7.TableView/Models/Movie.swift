291ad2497e50ce3a0261276fca348c50de98b9f0
JeongAYoo <jeonga038@naver.com>
2022-12-07 04:06:19 +0900

    Add TableView Delegate func, TableViewCell

---
+++ 7.TableView/7.TableView/Models/Movie.swift
@@ -0,0 +1,15 @@
+//
+//  Movie.swift
+//  7.TableView
+//
+//  Created by Jade Yoo on 2022/12/07.
+//
+
+import UIKit
+
+struct Movie {
+    var movieImage: UIImage?
+    let movieName: String
+    let movieDescription: String
+}
+
