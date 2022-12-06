ea1f2e3999d54130acde22ec6140751eefb594ce
JeongAYoo <jeonga038@naver.com>
2022-12-06 23:20:25 +0900

    Add TableView Project

---
+++ 7.TableView/7.TableView/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  7.TableView
+//
+//  Created by Jade Yoo on 2022/12/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
