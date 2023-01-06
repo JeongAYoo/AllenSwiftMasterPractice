7cb5cfe6a2dd51342e1e0bbcb07330cf9bece74d
JeongAYoo <jeonga038@naver.com>
2023-01-06 22:19:40 +0900

    Add Music TableViewCell

--- 9.MusicApp/9.MusicApp/Controllers/ViewController.swift
+++ 9.MusicApp/9.MusicApp/Controllers/ViewController.swift
@@ -30,11 +30,13 @@ class ViewController: UIViewController {
 
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
+        return 10
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = musicTableView.dequeueReusableCell(withIdentifier: Cell.musicCellIdentifier, for: indexPath)
         
+        return cell
     }
     
     
