44e14e369e3205b87f075e313ff76c89f562c748
JeongAYoo <jeonga038@naver.com>
2023-02-08 23:29:36 +0900

    SearchResultViewController

--- 9.MusicApp/9.MusicApp/Controllers/SearchResultViewController.swift
+++ 9.MusicApp/9.MusicApp/Controllers/SearchResultViewController.swift
@@ -7,13 +7,89 @@
 
 import UIKit
 
-class SearchResultViewController: UIViewController {
-
+final class SearchResultViewController: UIViewController {
+    // MARK: - Properties
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    // 컬렉션뷰의 레이아웃을 담당하는 객체
+    let flowLayout = UICollectionViewFlowLayout()
+    
+    // 네트워크 매니저 (싱글톤)
+    let networkManager = NetworkManager.shared
+    
+    var musicArrays: [Music] = []
+    
+    /// (서치바에서) 검색을 위한 단어를 담는 변수 (전화면에서 전달받음)
+    var searchTerm: String? {
+        didSet {
+            setupDatas()
+        }
+    }
+    
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .white
+        setupCollectionView()
     }
+    
+    // MARK: - Methods
+    func setupCollectionView() {
+        collectionView.dataSource = self
+        
+        collectionView.backgroundColor = .white
+        // 컬렉션뷰의 스크롤 방향 설정
+        flowLayout.scrollDirection = .vertical
+        
+        // 셀의 너비 = 화면 너비 - (줄간격 * 셀의 갯수 - 1) / 셀의 갯수
+        let collectionCellWidth = (UIScreen.main.bounds.width - CVCell.spacingWidth * (CVCell.cellColumns - 1)) / CVCell.cellColumns
+        
+        flowLayout.itemSize = CGSize(width: collectionCellWidth, height: collectionCellWidth)
+        // 아이템 사이 간격 설정
+        flowLayout.minimumInteritemSpacing = CVCell.spacingWidth
+        // 아이템 위아래 사이 간격 설정
+        flowLayout.minimumLineSpacing = CVCell.spacingWidth
+        
+        // 컬렉션뷰의 속성에 할당
+        collectionView.collectionViewLayout = flowLayout
+    }
+    
+    func setupDatas() {
+        guard let term = searchTerm else { return }
+        print("네트워킹 시작 단어: \(term)")
+        
+        // (네트워킹 시작전에) 다시 빈배열로 만들기
+        self.musicArrays = []
+        
+        // 네트워킹 시작 (찾고자하는 단어를 가지고)
+        networkManager.fetchMusic(searchTerm: term) { result in
+            switch result {
+            case .success(let musicDatas):
+                self.musicArrays = musicDatas
+                // 메인스레드에서 컬렉션뷰를 리로드
+                DispatchQueue.main.async {
+                    self.collectionView.reloadData()
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
 
-
+// MARK: - UICollectionView DataSource
+extension SearchResultViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return musicArrays.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Cell.musicCollectionViewCellIdentifier, for: indexPath) as! MusicCollectionViewCell
+        cell.imageUrl = musicArrays[indexPath.item].imageUrl
+        
+        return cell
+    }
+    
 }
--- 9.MusicApp/9.MusicApp/Controllers/ViewController.swift
+++ 9.MusicApp/9.MusicApp/Controllers/ViewController.swift
@@ -11,7 +11,10 @@ class ViewController: UIViewController {
     // MARK: - Properties
     
     // 서치 컨트롤러 생성 ==> 네비게이션 아이템에 할당
-    let searchController = UISearchController()
+//    let searchController = UISearchController()
+    
+    // 서치 Results 컨트롤러 ⭐️
+    let searchController = UISearchController(searchResultsController: UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "SearchResultViewController") as! SearchResultViewController)
     
     @IBOutlet weak var musicTableView: UITableView!
     
@@ -34,8 +37,14 @@ class ViewController: UIViewController {
     func setupSearchBar() {
         self.title = "Music Search"
         navigationItem.searchController = searchController
+        // 1) 단순 서치바의 사용
+        //searchController.searchBar.delegate = self
         
-        searchController.searchBar.delegate = self
+        // 2) 서치 Results 컨트롤러의 사용 (복잡한 구현 가능)
+        searchController.searchResultsUpdater = self
+        
+        // 첫글자 대문자 설정 없애기
+        searchController.searchBar.autocapitalizationType = .none
     }
     
     /// 테이블 뷰 세팅
@@ -99,16 +108,39 @@ extension ViewController: UITableViewDelegate {
 }
 
 // MARK: - UISearchBar Delegate (단순) 서치바 확장
-extension ViewController: UISearchBarDelegate {
-    
-//    // 유저가 글자를 입력하는 순간마다 호출되는 메서드
-//    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-//        print(searchText)
-//        // (검색결과를 보여줄 새로운 테이블을 만들거니까) 다시 빈 배열로 만들기⭐️
+//extension ViewController: UISearchBarDelegate {
+//
+////    // 유저가 글자를 입력하는 순간마다 호출되는 메서드
+////    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+////        print(searchText)
+////        // (검색결과를 보여줄 새로운 테이블을 만들거니까) 다시 빈 배열로 만들기⭐️
+////        self.musicArrays = []
+////
+////        // 네트워킹 시작
+////        networkManager.fetchMusic(searchTerm: searchText) { result in
+////            switch result {
+////            case .success(let musicDatas):
+////                self.musicArrays = musicDatas
+////                DispatchQueue.main.async {
+////                    self.musicTableView.reloadData()
+////                }
+////            case .failure(let error):
+////                print(error.localizedDescription)
+////            }
+////        }
+////    }
+//
+//    // 검색(Search) 버튼을 눌렀을 때 호출되는 메서드
+//    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+//        guard let text = searchController.searchBar.text else {
+//            return
+//        }
+//        print(text)
+//        // 다시 빈 배열로 만들기⭐️
 //        self.musicArrays = []
 //
 //        // 네트워킹 시작
-//        networkManager.fetchMusic(searchTerm: searchText) { result in
+//        networkManager.fetchMusic(searchTerm: text) { result in
 //            switch result {
 //            case .success(let musicDatas):
 //                self.musicArrays = musicDatas
@@ -119,29 +151,21 @@ extension ViewController: UISearchBarDelegate {
 //                print(error.localizedDescription)
 //            }
 //        }
+//        self.view.endEditing(true)
 //    }
+//}
+
+// MARK: - UISearchResultsUpdating
+extension ViewController: UISearchResultsUpdating {
     
-    // 검색(Search) 버튼을 눌렀을 때 호출되는 메서드
-    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        guard let text = searchController.searchBar.text else {
-            return
-        }
-        print(text)
-        // 다시 빈 배열로 만들기⭐️
-        self.musicArrays = []
+    // (필수) 유저가 글자를 입력하는 순간마다 호출되는 메서드 ==> 일반적으로 다른 화면을 보여줄 때 구현
+    func updateSearchResults(for searchController: UISearchController) {
+        //print("서치바에 입력되는 단어", searchController.searchBar.text)
         
-        // 네트워킹 시작
-        networkManager.fetchMusic(searchTerm: text) { result in
-            switch result {
-            case .success(let musicDatas):
-                self.musicArrays = musicDatas
-                DispatchQueue.main.async {
-                    self.musicTableView.reloadData()
-                }
-            case .failure(let error):
-                print(error.localizedDescription)
-            }
-        }
-        self.view.endEditing(true)
+        // 글자를 치는 순간에 다른 화면을 보여주고 싶다면 (컬렉션뷰를 보여줌)
+        let vc = searchController.searchResultsController as! SearchResultViewController
+        // 컬렉션뷰에 찾으려는 단어 전달
+        vc.searchTerm = searchController.searchBar.text ?? ""
     }
+    
 }
